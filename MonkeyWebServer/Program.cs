11a2294d128c7bee080f953b13b6f52f1a146051
xVice <justus.hanschi@gmail.com>
2023-06-30 21:51:31 +0200

    Added "error handling" and debug bool.

--- MonkeyWebServer/Program.cs
+++ MonkeyWebServer/Program.cs
@@ -1,12 +1,9 @@
 ﻿using MonkeyWebServer;
 using Newtonsoft.Json;
 using Scriban;
-using System;
 using System.Dynamic;
-using System.IO;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 
 public class Prog
 {
@@ -15,7 +12,7 @@ public class Prog
         MonkeyEndpoints.AddEndpoint(new ExampleEndpoint());
         MonkeyEndpoints.AddEndpoint(new ExampleJsonEndpoint());
 
-        MonkeyServer.StartServer();
+        MonkeyServer.StartServer(true);
         MonkeyServer.AddPrefix("http://localhost:8000/");
         Task.Run(MonkeyServer.HandleIncomingConnections);
 
@@ -67,32 +64,28 @@ public class ExampleJsonEndpoint : MonkeyEndpoint
 
 
 #region MonkeyServer
-public abstract class MonkeyEndpoint
-{
-    public abstract string Endpoint { get; }
-
-    public abstract MonkeyResponse Execute(HttpListenerContext ctx);
-}
-
-public static class MonkeyEndpoints
-{
-    public static List<MonkeyEndpoint> EndPoints = new List<MonkeyEndpoint>();
-
-    public static void AddEndpoint(MonkeyEndpoint endpoint)
-    {
-        EndPoints.Add(endpoint);
-    }
-}
-
 public static class MonkeyServer
 {
     public static HttpListener listener;
     public static string url = "http://localhost:8000/";
     private static bool runServer = false;
+    private static bool isDebug = false;
+
+    public static void StartServer(bool dbg)
+    {
+        isDebug = dbg;
+        if (runServer == false)
+        {
+            listener = new HttpListener();
+            listener.Start();
+            Console.WriteLine($"Listening for requests on {url}");
+            runServer = true;
+        }
+    }
 
     public static void StartServer()
     {
-        if(runServer == false)
+        if (runServer == false)
         {
             listener = new HttpListener();
             listener.Start();
@@ -127,22 +120,57 @@ public static class MonkeyServer
 
             if (endpoint != null)
             {
-        
-                // Execute the endpoint and send the response
-                Logger.LogIncoming($"Handling endpoint: §2{urlPath}");
-                MonkeyResponse monkeyresponse = endpoint.Execute(context);
-                await monkeyresponse.response.OutputStream.WriteAsync(monkeyresponse.responseData, 0, monkeyresponse.responseData.Length);
-                monkeyresponse.response.Close();
+                try
+                {
+                    // Execute the endpoint and send the response
+                    Logger.LogIncoming($"Handling endpoint: §2{urlPath}");
+                    MonkeyResponse monkeyresponse = endpoint.Execute(context);
+                    await monkeyresponse.response.OutputStream.WriteAsync(monkeyresponse.responseData, 0, monkeyresponse.responseData.Length);
+                    monkeyresponse.response.Close();
+                }
+                catch(Exception ex)
+                {
+                    Logger.LogIncoming($"Error encountered while handling the endpoint: §4{urlPath}");
+                    if (isDebug)
+                    {
+                        dynamic error = new ExpandoObject();
+                        error.message = ex.Message;
+                        error.endpoint = urlPath;
+                        MonkeyResponse monkeyresponseerr = MonkeyResponse.RenderTemplate(context.Response, "./internal/monkey-error.html", new { Error = error });
+                        await monkeyresponseerr.response.OutputStream.WriteAsync(monkeyresponseerr.responseData, 0, monkeyresponseerr.responseData.Length);
+                        monkeyresponseerr.response.Close();
+
+                    }
+                    else
+                    {
+                        MonkeyResponse monkeyresponseerr2 = MonkeyResponse.Text(context.Response, "Error 500", 500);
+                        await monkeyresponseerr2.response.OutputStream.WriteAsync(monkeyresponseerr2.responseData, 0, monkeyresponseerr2.responseData.Length);
+                        monkeyresponseerr2.response.Close();
+                    }
+
+
+                }
+
             }
             else
             {
-                Logger.LogIncoming($"Coudlnt find endpoint: §4{urlPath}, §8did you register it using §6MonkeyEndpoints§8.§2AddEndpoint§12()§8?");
-                dynamic error = new ExpandoObject();
-                error.message = "Your team of monkeys couldnt find the endpoint you just tried to navigate to, Did you register it using: MonkeyEndpoints.AddEndpoint()?";
-                error.endpoint = urlPath;
-                MonkeyResponse resp = MonkeyResponse.RenderTemplate(context.Response, "./internal/monkey-error.html", new { Error = error });
-                await resp.response.OutputStream.WriteAsync(resp.responseData, 0, resp.responseData.Length);
-                context.Response.Close();
+                if (isDebug)
+                {
+                    Logger.LogIncoming($"Coudlnt find endpoint: §4{urlPath}, §8did you register it using §6MonkeyEndpoints§8.§2AddEndpoint§12()§8?");
+                    dynamic error = new ExpandoObject();
+                    error.message = "Your team of monkeys couldnt find the endpoint you just tried to navigate to, Did you register it using: MonkeyEndpoints.AddEndpoint()?";
+                    error.endpoint = urlPath;
+                    MonkeyResponse resp = MonkeyResponse.RenderTemplate(context.Response, "./internal/monkey-error.html", new { Error = error });
+                    await resp.response.OutputStream.WriteAsync(resp.responseData, 0, resp.responseData.Length);
+                    context.Response.Close();
+                }
+                else
+                {
+                    MonkeyResponse monkeyresponseerr2 = MonkeyResponse.Text(context.Response, "Error 500", 500);
+                    await monkeyresponseerr2.response.OutputStream.WriteAsync(monkeyresponseerr2.responseData, 0, monkeyresponseerr2.responseData.Length);
+                    monkeyresponseerr2.response.Close();
+                }
+
             }
         }
     }
@@ -153,6 +181,25 @@ public static class MonkeyServer
     }
 }
 
+public abstract class MonkeyEndpoint
+{
+    public abstract string Endpoint { get; }
+
+    public abstract MonkeyResponse Execute(HttpListenerContext ctx);
+}
+
+public static class MonkeyEndpoints
+{
+    public static List<MonkeyEndpoint> EndPoints = new List<MonkeyEndpoint>();
+
+    public static void AddEndpoint(MonkeyEndpoint endpoint)
+    {
+        EndPoints.Add(endpoint);
+    }
+}
+
+
+
 public class MonkeyResponse
 {
     public HttpListenerResponse response;
@@ -162,6 +209,30 @@ public class MonkeyResponse
     {
         this.response = response;
         this.responseData = responseData;
+    }
+
+    public static MonkeyResponse Text(HttpListenerResponse response, string text, int statusCode)
+    {
+        byte[] data = Encoding.UTF8.GetBytes(text);
+
+        response.StatusCode = statusCode;
+        response.ContentType = "text/plain";
+        response.ContentEncoding = Encoding.UTF8;
+        response.ContentLength64 = data.LongLength;
+
+        return new MonkeyResponse(response, data);
+    }
+
+    public static MonkeyResponse Text(HttpListenerResponse response, string text)
+    {
+        byte[] data = Encoding.UTF8.GetBytes(text);
+
+        response.StatusCode = 200;
+        response.ContentType = "application/json";
+        response.ContentEncoding = Encoding.UTF8;
+        response.ContentLength64 = data.LongLength;
+
+        return new MonkeyResponse(response, data);
     }
 
     public static MonkeyResponse Json(HttpListenerResponse response, object jsonData)
